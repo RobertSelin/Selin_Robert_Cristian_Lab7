555826ecd62b92b6c31380394675fe9fb1ea1c53
DESKTOP-TUILT7P\Robi <robert.selin@yahoo.com>
2022-12-12 22:55:57 +0200

    Lab9

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -17,6 +17,8 @@ namespace Selin_Robert_Cristian_Lab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
 
         public Task<List<ShopList>> GetShopListsAsync()
@@ -46,6 +48,49 @@ namespace Selin_Robert_Cristian_Lab7.Data
         public Task<int> DeleteShopAsync(ShopList slist)
         {
             return _database.DeleteAsync(slist);
+        }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }    
+        }
+
+        public Task<List<Product>> GetListProductAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+                "select P.ID, P.Description from Product P"
+                + " inner join ListProduct LP"
+                + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+                shoplistid);
         }
     }
 }
--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -23,4 +23,20 @@ public partial class ListPage : ContentPage
 		await App.Database.DeleteShopAsync(slist);
 		await Navigation.PopAsync();
 	}
+
+	async void OnChooseButtonClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new ProductPage((ShopList)
+			this.BindingContext)
+		{
+			BindingContext = new Product()
+		});
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		var shopl = (ShopList)BindingContext;
+		listView.ItemsSource = await App.Database.GetListProductAsync(shopl.ID);
+	}
 }
