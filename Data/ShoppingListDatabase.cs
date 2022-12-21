640c76e7030f0b17f8414a9613b609f4c6c4eeb5
DESKTOP-TUILT7P\Robi <robert.selin@yahoo.com>
2022-12-21 13:26:33 +0200

    Lab10

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -19,6 +19,7 @@ namespace Selin_Robert_Cristian_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
 
         public Task<List<ShopList>> GetShopListsAsync()
@@ -92,6 +93,24 @@ namespace Selin_Robert_Cristian_Lab7.Data
             + " on P.ID = LP.ProductID where LP.ShopListID = ?",
             shoplistid);
         }
+
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
+
 
     }
 }
